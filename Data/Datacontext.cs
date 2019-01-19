5bd1f190e3f1b343dae22241fadaf06eaac12320
ishanbhawantha <ishanbhawantha@gmail.com>
2019-01-19 21:42:36 +0530

    Initial Commit

---
+++ Data/Datacontext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> options) : base(options)
+        {
+                   
+        }
+        public DbSet <Value> Values { get; set; }
+    }
+}
